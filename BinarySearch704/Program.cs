54d1b1f6a42d59cb3ecfb2a7f2e9f9472187a03e
Arman Aslanyan <armo2004.16@gmail.com>
2025-08-13 13:11:13 +0400

    BinarySearch solved

---
+++ BinarySearch704/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    public void Main()
+    {
+        
+    }
+
+    public static int Search(int[] arr, int target)
+    {
+        
+
+        return 1;
+    }
+}
