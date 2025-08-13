2135468de9d63bc56a6d821642f47482dc7f02c1
Arman Aslanyan <armo2004.16@gmail.com>
2025-08-13 15:23:08 +0400

    Fixed BinarySearch

--- BinarySearch704/Program.cs
+++ BinarySearch704/Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 class Program
 {
-    public void Main()
+    public static void Main()
     {
-        
+        int[] arr = [-1, 0, 3, 5, 9, 12];
+        Console.WriteLine(Search(arr, 9));
     }
 
     public static int Search(int[] arr, int target)
     {
-        
+        int low = 0;
+        int high = arr.Length - 1;
+        while (low <= high)
+        {
+            int middle = (low + high) / 2;
+            if (arr[middle] == target)
+                return middle;
+            else if (arr[middle] < target)
+                low = middle + 1;
+            else
+                high = middle - 1;
+        }
 
-        return 1;
+        return -1;
     }
 }
--- BinarySearch704/obj/Debug/net8.0/BinarySearch704.AssemblyInfo.cs
+++ BinarySearch704/obj/Debug/net8.0/BinarySearch704.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BinarySearch704")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d1b1f6a42d59cb3ecfb2a7f2e9f9472187a03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BinarySearch704")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BinarySearch704")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
