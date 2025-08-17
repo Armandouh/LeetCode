1a9074d3f47dc49d286d3011cd72aed22214f598
Arman Aslanyan <armo2004.16@gmail.com>
2025-08-17 18:19:00 +0400

    Solved KokoEatingBananas and FindMinInSortedRotatedArray

---
+++ FindMinInRotatedArr153/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        int[] nums = { 3, 4, 5, 1, 2 };
+        Console.WriteLine(FindMin(nums));
+    }
+
+    static public int FindMin(int[] nums)
+    {
+        int l = 0;
+        int r = nums.Length - 1;
+        int res = int.MaxValue;
+
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            res = Math.Min(nums[m], res);
+
+            if (nums[l] < nums[r])
+            {
+                res = Math.Min(nums[l], res);
+                break;  
+            }
+            
+            else if (nums[m] >= nums[l])
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return res;
+    }
+}
---
+++ KokoEatingBananas875/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        //int[] arr = { 3, 6, 7, 11 };
+        //Console.WriteLine(minEatingSpeed(arr, 8));
+
+        int[] piles = { 30, 11, 23, 4, 20 };
+        Console.WriteLine(minEatingSpeed(piles, 5));
+        
+        //int[] piles2 = { 30, 11, 23, 4, 20 };
+        //Console.WriteLine(minEatingSpeed(piles2, 6));
+    }
+
+    public static int minEatingSpeed(int[] piles, int h)
+    {
+        int k = 1;
+
+        int l = 1;
+        int r = piles.Max();
+
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            if (canEat(piles, h, m))
+            {
+                k = m;
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return k;
+    }
+
+    public static bool canEat(int[] piles, int h, int k)
+    {
+        double total = 0;
+        foreach (int n in piles)
+        {
+            double hour = (double)n / k;
+            double rounded = Math.Ceiling(hour);
+            total += rounded;
+        }
+
+        return total <= h;
+    }
+}
