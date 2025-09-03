0ba975482d409e29306eab7e4d31b4ef4aaee6a0
Arman Aslanyan <armo2004.16@gmail.com>
2025-09-03 15:06:44 +0400

    Solved ReverseList206 and LinkedListCycle141

---
+++ LinkedListCycle141/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+
+        first.next = second;
+        second.next = third;
+
+        Console.WriteLine(HasCycle(first));
+    }
+
+    public static bool HasCycle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        
+        return false;
+    }
+}
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
---
+++ ReverseList206/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+class Program
+{
+    static public void Main()
+    {
+        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3)));
+        var curr = list;
+    }
+
+    static ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null)
+        {
+            var next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
